6ca06a5f2eec003675aee2ae8b2774661c4ff6b3
Ethan Brouwer <me@ebrouwer.dev>
2020-09-30 11:47:13 -0600

    Scraping works!

--- scraping/scrape_athletes.py
+++ scraping/scrape_athletes.py
@@ -1,14 +1,26 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from bs4 import BeautifulSoup
 import time
 from urllib import parse as urlparser
 import json
+import asyncio
+from concurrent.futures.thread import ThreadPoolExecutor
+
+chrome_options = Options()
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--window-size=1920x1080")
+
+# GLOBAL OPTIONS
+BEGIN = 1
+END = 1000
+WORKERS = 10
 
 
 def get_athlete_soup(athlete_id):
-    browser = webdriver.Chrome()
+    browser = webdriver.Chrome(options=chrome_options)
     browser.get(f'https://www.digitalrock.de/egroupware/ranking/sitemgr/digitalrock/pstambl.html#person={athlete_id}')
-    time.sleep(2.5)
+    time.sleep(3)
     soup = BeautifulSoup(browser.page_source, 'html.parser')
     browser.close()
     return soup
@@ -38,21 +50,52 @@ def get_athlete_comps(soup):
     return list(map(parse_result, results))
 
 
-if __name__ == '__main__':
-    athletes_data = []
-    athletes_comps = {}
-    for i in range(1, 11):
-        try:
-            soup = get_athlete_soup(i)
-            profile = soup.find(id='profile')
-            if profile.contents:
-                athletes_data.append(get_athlete_data(soup))
-                athletes_comps[i] = get_athlete_comps(soup)
-        except Exception as e:
-            print(f'Error while processing id: {i}')
-            print(e)
+def scrape(i):
+    print(f'Scraping id: {i}')
+    try:
+        soup = get_athlete_soup(i)
+        profile = soup.find(id='profile')
+        if profile and profile.contents:
+            data = get_athlete_data(soup)
+            comps = get_athlete_comps(soup)
+            return i, data, comps
+    except Exception as e:
+        print(f'Error while processing id: {i}')
+        print(e)
 
+
+async def run_async_tasks(loop, func, i_list):
+    with ThreadPoolExecutor(WORKERS) as executor:
+        result = []
+        for response in await asyncio.gather(*[loop.run_in_executor(executor, func, i) for i in i_list]):
+            result.append(response)
+        return result
+
+
+def main():
+    try:
+        loop = asyncio.get_event_loop()
+    except Exception as e:
+        print(e)
+        asyncio.set_event_loop(asyncio.new_event_loop())
+        loop = asyncio.get_event_loop()
+    start = time.time()
+    results = list(filter(lambda x: x, loop.run_until_complete(asyncio.ensure_future(run_async_tasks(loop, scrape, range(BEGIN, END+1))))))
+    print(f'Finished scraping in {time.time() - start} seconds')
+
+    athletes_data = {}
+    athletes_comps = {}
+    for i, data, comps in results:
+        athletes_data[i] = data,
+        athletes_comps[i] = comps
+
+    start = time.time()
     with open('../data/athlete_data.json', 'w+') as f:
         f.write(json.dumps(athletes_data))
-    with open('../data/authlete_comps.json', 'w+') as f:
+    with open('../data/athlete_comps.json', 'w+') as f:
         f.write(json.dumps(athletes_comps))
+    print(f'Finished writing to files in {time.time() - start} seconds')
+
+
+if __name__ == '__main__':
+    main()
